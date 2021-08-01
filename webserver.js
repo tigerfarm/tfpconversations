47d0757d441d3f40dcd6e5c3213ca5c7617987d2
Stacy David Thurston <tigerfarm@gmail.com>
2021-08-01 13:12:14 -0700

    update

--- webserver.js
+++ webserver.js
@@ -14,7 +14,7 @@
 // 
 // Create environment variables which are used in the generateToken() function.
 //
-var ACCOUNT_SID = process.env.MASTER_ACCOUNT_SID;
+var ACCOUNT_SID = process.env.CONVERSATIONS_ACCOUNT_SID;
 //
 // Create a Chat Service:
 //  https://www.twilio.com/console/chat/dashboard
@@ -22,11 +22,11 @@ var CONVERSATIONS_SERVICE_SID = process.env.CONVERSATIONS_SERVICE_SID;
 //
 // Create an API key and secret string:
 //  https://www.twilio.com/console/chat/runtime/api-keys
-var API_KEY = process.env.API_KEY;
-var API_KEY_SECRET = process.env.API_KEY_SECRET;
+var API_KEY = process.env.CONVERSATIONS_API_KEY;
+var API_KEY_SECRET = process.env.CONVERSATIONS_API_KEY_SECRET;
 // -----------------------------------------------------------------------------
 console.log("+++ Chat program is starting up.");
-var client = require('twilio')(process.env.MASTER_ACCOUNT_SID, process.env.MASTER_AUTH_TOKEN);
+var client = require('twilio')(process.env.CONVERSATIONS_ACCOUNT_SID, process.env.CONVERSATIONS_ACCOUNT_AUTH_TOKEN);
 // -----------------------------------------------------------------------------
 var returnMessage = '';
 function sayMessage(message) {
@@ -97,7 +97,7 @@ function addParticipantToConversation(res, conversationId, participantIdentity) 
 const express = require('express');
 const path = require('path');
 const url = require("url");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.CONVERSATIONS_PORT || 8000;
 var app = express();
 // -----------------------------------------------------------------------------
 app.get('/generateToken', function (req, res) {
@@ -154,7 +154,7 @@ app.get('/joinConversation', function (req, res) {
                         console.log("+ Conversation does NOT exit, create it.");
                         client.conversations.services(CONVERSATIONS_SERVICE_SID).conversations
                                 .create({
-                                    messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
+                                    messagingServiceSid: process.env.CONVERSATIONS_MESSAGING_SERVICE_SID,
                                     uniqueName: conversationId,
                                     friendlyName: conversationId
                                 })
