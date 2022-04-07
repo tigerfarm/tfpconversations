1f2fbc3f5c7386f7ed270154a83f443abc823dda
Stacy David Thurston <tigerfarm@gmail.com>
2022-04-07 15:51:40 -0700

    update

--- src/providers/customers.js
+++ src/providers/customers.js
@@ -50,7 +50,7 @@ const customers = [
         display_name: 'Percy Byshee Shelley',
         channels: [
             {type: 'sms', value: '+16503790007'},
-            {type: 'sms', value: '+16508661366'}
+            // {type: 'sms', value: '+16508661366'}
         ],
         worker: 'tigerfarm@gmail.com',
         avatar: 'https://someassets-1403.twil.io/Shelley.jpg'
