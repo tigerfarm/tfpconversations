cb1d93389a84d9978958d4fb8f0b38a8bbf16742
Stacy David Thurston <tigerfarm@gmail.com>
2022-09-13 13:07:06 -0700

    update

--- src/routes/callbacks/crm.js
+++ src/routes/callbacks/crm.js
@@ -1,32 +1,13 @@
 const { getCustomerById, getCustomersList } = require('../../providers/customers');
 /*
 + POST URL : https://example.com/callbacks/crm
---------------
-+ Headers:
-++ 0: "host":"example.herokuapp.com"
-++ 1: "connection":"close"
-++ 2: "content-type":"application/x-www-form-urlencoded; charset=UTF-8"
-++ 3: "i-twilio-idempotency-token":"7369b88c-197c-415d-b310-086342ecd11d"
-++ 4: "x-twilio-signature":"0r0eXrqoy8Sd5GOlTNoonHccZiY="
-++ 5: "accept":"* / *"
-++ 6: "x-home-region":"us1"
-++ 7: "user-agent":"TwilioProxy/1.1"
-++ 8: "x-request-id":"9b8003ae-2dda-45cf-968e-150b7060b7ca"
-++ 9: "x-forwarded-for":"3.81.123.155"
-++ 10: "x-forwarded-proto":"https"
-++ 11: "x-forwarded-port":"443"
-++ 12: "via":"1.1 vegur"
-++ 13: "connect-time":"0"
-++ 14: "x-request-start":"1663030127165"
-++ 15: "total-route-time":"0"
-++ 16: "content-length":"66"
 POST Content ---------------------------------
 + Raw : 
-PageSize=30&Worker=tigerfarm%40gmail.com&Location=GetCustomersList
+PageSize=30&Worker=dave%40example.com&Location=GetCustomersList
 POST Content ---------------------------------
 + Name value pairs: 
    "PageSize": "30",
-   "Worker": "tigerfarm@gmail.com",
+   "Worker": "dave@example.com",
    "Location": "GetCustomersList",
  */
 
@@ -71,11 +52,11 @@ const handleGetCustomerDetailsByCustomerIdCallback = async (req, res) => {
   display_name: 'Percy Byshee Shelley',
   company_name: 'Poets Inc',
   channels: [
-    { type: 'sms', value: '+16503790007' },
-    { type: 'sms', value: '+16508661366' }
+    { type: 'sms', value: '+16505551111' },
+    { type: 'sms', value: '+16505552222' }
   ],
   worker: 'tigerfarm@gmail.com',
-  avatar: 'https://someassets-1403.twil.io/Shelley.jpg'
+  avatar: 'https://abouttime-2357.twil.io/Shelley.jpg'
 }
  */
     // Respond with Contact object
@@ -106,22 +87,22 @@ const handleGetCustomersListCallback = async (req, res) => {
   {
     display_name: 'Coleridge',
     customer_id: 1,
-    avatar: 'https://someassets-1403.twil.io/Coleridge.jpg'
+    avatar: 'https://abouttime-2357.twil.io/Coleridge.jpg'
   },
   {
     display_name: 'Percy Byshee Shelley',
     customer_id: 2,
-    avatar: 'https://someassets-1403.twil.io/Shelley.jpg'
+    avatar: 'https://abouttime-2357.twil.io/Shelley.jpg'
   },
   {
     display_name: 'John Keats',
     customer_id: 3,
-    avatar: 'https://someassets-1403.twil.io/Keats.jpg'
+    avatar: 'https://abouttime-2357.twil.io/Keats.jpg'
   },
   {
     display_name: 'Dave here',
     customer_id: 33,
-    avatar: 'https://someassets-1403.twil.io/avatarMine1.jpg'
+    avatar: 'https://abouttime-2357.twil.io/avatarMine1.jpg'
   }
 ]
     */
